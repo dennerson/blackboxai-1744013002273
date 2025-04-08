8a1194baf22d95fc3e270304ed84c830190e6837
dennerson <39244494+dennerson@users.noreply.github.com>
2025-04-08 08:12:45 +0800

    Add ecommerce-app/postcss.config.js 1744071164162

--- ecommerce-app/postcss.config.js
+++ ecommerce-app/postcss.config.js
@@ -1,6 +1,8 @@
 module.exports = {
-  plugins: {
-    tailwindcss: {},
-    autoprefixer: {},
-  },
+    plugins: {
+        '@tailwindcss/postcss': {
+            config: './tailwind.config.js'
+        },
+        autoprefixer: {},
+    },
 }
